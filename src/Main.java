efa135ab3b80040412d5cc88da07973d19c150fd
7silasmelo7 <silascooll@gmail.com>
2025-12-13 14:44:54 -0300

    Conteúdo finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.desafio.dominio.Bootcamp;
 import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Dev;
 import br.com.desafio.dominio.Mentoria;
 
 public class Main {
@@ -20,9 +22,43 @@ public class Main {
         mentoria.setData(java.time.LocalDate.now());
 
 
-        System.out.println(curso1);
+       /* System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria); */
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devSilas = new Dev();
+        devSilas.setNome("Silas");
+        devSilas.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Silas: " + devSilas.getConteudosInscritos());
+
+
+        devSilas.progredir();
+        devSilas.progredir();
+        devSilas.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Silas: " + devSilas.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Silas: " + devSilas.getConteudosConcluidos());
+        System.out.println("XP: " + devSilas.calcularTotalXp());
+
+        System.out.println("--------------");
+
+        Dev devCarol = new Dev();
+        devCarol.setNome("Carol");
+        devCarol.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Carol: " + devCarol.getConteudosInscritos());
+        devCarol.progredir();
+        devCarol.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Carol: " + devCarol.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Carol: " + devCarol.getConteudosConcluidos());
+        System.out.println("XP: " + devCarol.calcularTotalXp());
 
     }
 }
